f51851cb0b07f51977ea486d3424e1b01af71ca8
Marie <marieglahn@hotmail.com>
2014-07-30 16:09:10 +0200

    Added untracked files

---
+++ javascripts/images-masonry-grid.js
@@ -0,0 +1,42 @@
+
+// initialize Masonry
+var $container = $('#masonry-container').masonry();
+
+// layout Masonry again after all images have loaded
+// NB. Potential problem when introducing infinite scroll?
+  $container.imagesLoaded( function() {
+      $container.masonry({
+          itemSelector: '.masonry-item',
+          "gutter": 15,
+          cornerStampSelector: '.corner-stamp',
+      });
+
+});
+
+//var msnry = $container.data('masonry');
+
+//Hover shows opaque overlay on masonry item image with links:
+$(function() {
+  $('.text-content').hide();
+  $('.masonry-item').hover(function() {
+       var $image = $(this).find($(".masonry-image"));
+       var $height = $image.height();
+       var $overlay = $( this ).find( ".text-content" );
+       $overlay.toggle();
+       $overlay.css("height" , $height);
+   });
+});
+
+
+//Vertical menu:
+$('.images-sub-menu').hide(); //Hide sub menu by default
+
+//On click show/hide sub items
+$('.brands-categories-title').click(function(event){
+    if ($(this).siblings().is(":visible")) {
+        $(this).siblings().hide();
+    } else {
+        $('.images-sub-menu').hide(); //Hide sub menu 
+        $(this).siblings().show();
+    }
+});  
